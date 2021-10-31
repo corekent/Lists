cebb1a768db4d45172cbf720242854d183fd3413
Алиса Старцева <alisafoxpirs@gmail.com>
2021-11-01 00:23:00 +0300

    getting started with lists

---
+++ ArrayList/ArrayList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArrayList
+{
+    public class ArrayList
+    {
+        public int Length;
+        private int[] _array;
+
+        public ArrayList()
+        {
+            Length = 0;
+            _array = new int[10];
+        }
+        public ArrayList(int value)
+        {
+            Length = 1;
+            _array[0] = value;
+        }
+        public ArrayList(int[] value)
+        {
+            Length = value.Length;
+            _array = value;
+        }
+
+
+    }
+}
---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    public class LinkedList
+    {        
+        private Node _head;
+        private Node _tail;
+
+        public LinkedList()
+        {
+            _head = null;
+            _head = null;
+        }
+        public LinkedList(int value)
+        {
+            _head = new Node(value);
+            _tail = _head;
+        }
+        public LinkedList(int[] value)
+        {
+
+        }
+
+        
+
+        public int this[int index]
+        {
+            get
+            {
+                Node current = _head;
+                for (int i = 1; i <= index; i++)
+                {
+                    current = current.Next;
+                }
+                return current.Value;
+            }
+
+            set
+            {
+                Node current = _head;
+                for (int i = 1; i <= index; i++)
+                {
+                    current = current.Next;
+                }
+                current.Value = value;
+            }
+        }
+
+        public void AddFirst(int val)
+        {
+            Node node = new Node(val);
+        }
+        
+
+        
+        
+    }
+}
+
+
+
